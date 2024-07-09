b52a45173d7f24eb3ec38b6a0d31fe688b594322
Agbor Kima <agborkima1@gmail.com>
2024-07-09 15:46:50 -0500

    added search bar

--- main.js
+++ main.js
@@ -2,9 +2,16 @@ const API_KEY  = 'QaiarjvvZTcfBKFna7Ry3B8SVut9eUmj'
 
 const newGifBtn = document.getElementById('new-gif-btn')
 const img = document.getElementById('gif-img')
+const search = document.getElementById('search')
 
 function getImage(){
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=skateboarding`)
+    let url
+    if(search.value === ''){
+        url = `https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=empty`
+    }else {
+        url = `https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=${search.value}`
+    }
+    fetch(url)
     .then(res => res.json())
     .then(data => img.src = (data.data.images.original.url))
 }
